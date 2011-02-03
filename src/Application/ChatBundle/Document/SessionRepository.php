ba70b1f58b0977a42ce0da901e2b6f0d2c811dd3
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-02-03 21:57:46 -0200

    call to createQueryBuilder from $this

--- src/Application/ChatBundle/Document/SessionRepository.php
+++ src/Application/ChatBundle/Document/SessionRepository.php
@@ -15,23 +15,27 @@ class SessionRepository extends DocumentRepository
 
     public function getRequestedChats()
     {
-        return $this->getDocumentManager()->createQueryBuilder('ChatBundle:Session')->field('updatedAt')->range(new MongoDate(time() - 300), new MongoDate(time()))->getQuery()->execute();
+        return $this->createQueryBuilder()->field('updatedAt')->range(new MongoDate(time() - 300), new MongoDate(time()))->getQuery()->execute();
     }
 
     public function closeSessions()
     {
-        $this->getDocumentManager()->createQueryBuilder('ChatBundle:Session')
-                ->field('statusId')->set(Session::STATUS_CANCELED)
-                ->field('statusId')->notIn(array(Session::STATUS_CANCELED, Session::STATUS_CLOSED))
-                ->field('updatedAt')->lt(new MongoDate(time() - 300))->getQuery()->execute();
+        $this->createQueryBuilder()->field('statusId')->set(Session::STATUS_CANCELED)->field('statusId')->notIn(array(
+            Session::STATUS_CANCELED,
+            Session::STATUS_CLOSED))->field('updatedAt')->lt(new MongoDate(time() - 300))->update()->getQuery()->execute();
     }
 
     public function getSessionIfNotFinished($id)
     {
-        return $this->getDocumentManager()->createQueryBuilder('ChatBundle:Session')
-                ->field('id')->equals($id)
-                ->field('statusId')->notIn(array(Session::STATUS_CANCELED, Session::STATUS_CLOSED))
-                ->field('updatedAt')->gt(new MongoDate(time() - 300))->getQuery()->getSingleResult();
+        return $this->createQueryBuilder()->field('id')->equals($id)->field('statusId')->notIn(array(
+            Session::STATUS_CANCELED,
+            Session::STATUS_CLOSED))->field('updatedAt')->gt(new MongoDate(time() - 300))->getQuery()->getSingleResult();
+    }
+
+    public function getOpenInvites()
+    {
+        return $this->find(array(
+            'statusId' => Session::STATUS_INVITE));
     }
 
 }
