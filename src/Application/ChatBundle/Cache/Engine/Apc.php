d6bae2760332dd498af1f271adb1d54e0d4d4566
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-02-19 20:11:08 -0200

    Mini cache api with APC support

---
+++ src/Application/ChatBundle/Cache/Engine/Apc.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\ChatBundle\Cache\Engine;
+
+use Application\ChatBundle\Cache\Cacheable;
+
+/**
+ * Description of Apc
+ *
+ * @author Ismael Ambrosi<ismael@servergrove.com>
+ */
+class Apc extends Base
+{
+
+    public function set($key, $var, $ttl = self::DEFAULT_TTL)
+    {
+        return apc_store($key, $var, $ttl);
+    }
+
+    public function get($key, $default = null)
+    {
+        if ($this->has($key)) {
+            return apc_fetch($key);
+        }
+
+        return $default;
+    }
+
+    public function has($key)
+    {
+        return apc_exists($key);
+    }
+
+    public function remove($key)
+    {
+        return apc_delete($key);
+    }
+
+}
---
+++ src/Application/ChatBundle/Cache/Engine/Base.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Application\ChatBundle\Cache\Engine;
+
+use Application\ChatBundle\Cache\Cacheable;
+
+/**
+ * Description of Base
+ *
+ * @author Ismael Ambrosi<ismael@servergrove.com>
+ */
+abstract class Base implements Cacheable
+{
+
+}
