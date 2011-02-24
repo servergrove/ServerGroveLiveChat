fa05418ecd1a6ce26e6955167db5d4096e178ee0
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-02-24 07:49:19 -0200

    Removed update time check in the function getSessionIfNotFinished of SessionRepository The function getSessionIfNotFinished of SessionRepository now closes sessions that is not waiting, closed or cancelled.

--- src/ServerGrove/SGLiveChatBundle/Document/SessionRepository.php
+++ src/ServerGrove/SGLiveChatBundle/Document/SessionRepository.php
@@ -21,32 +21,34 @@ class SessionRepository extends DocumentRepository
     public function getRequestedChatsArray()
     {
         return array_map(
-                function (Session $chat) {
-                    $operator = array();
-                    if ($chat->getOperator()) {
-                        $operator['id'] = $chat->getOperator()->getId();
-                        $operator['name'] = $chat->getOperator()->getName();
-                    }
-                    return array(
-                        'id' => $chat->getId(),
-                        'visitor' => array(
-                            'id' => $chat->getVisitor()->getId(),
-                            'name' => $chat->getVisitor()->getName(),
-                            'email' => $chat->getVisitor()->getEmail()),
-                        'question' => $chat->getQuestion(),
-                        'time' => $chat->getCreatedAt()->format('Y-m-d H:i:s'),
-                        'duration' => $chat->getUpdatedAt()->format('U') - $chat->getCreatedAt()->format('U'),
-                        'operator' => $operator,
-                        'status' => array(
-                            'id' => $chat->getStatusId(),
-                            'name' => $chat->getStatus()));
-                }, $this->getRequestedChats()->toArray());
+        function (Session $chat)
+        {
+            $operator = array();
+            if ($chat->getOperator()) {
+                $operator['id'] = $chat->getOperator()->getId();
+                $operator['name'] = $chat->getOperator()->getName();
+            }
+            return array(
+                'id' => $chat->getId(),
+                'visitor' => array(
+                    'id' => $chat->getVisitor()->getId(),
+                    'name' => $chat->getVisitor()->getName(),
+                    'email' => $chat->getVisitor()->getEmail()),
+                'question' => $chat->getQuestion(),
+                'time' => $chat->getCreatedAt()->format('Y-m-d H:i:s'),
+                'duration' => $chat->getUpdatedAt()->format('U') - $chat->getCreatedAt()->format('U'),
+                'operator' => $operator,
+                'status' => array(
+                    'id' => $chat->getStatusId(),
+                    'name' => $chat->getStatus()));
+        }, $this->getRequestedChats()->toArray());
     }
 
     public function closeSessions()
     {
         $this->createQueryBuilder()->field('statusId')->set(Session::STATUS_CANCELED)->field('statusId')->notIn(array(
             Session::STATUS_CANCELED,
+            Session::STATUS_WAITING,
             Session::STATUS_CLOSED))->field('updatedAt')->lt(new MongoDate(time() - 300))->update()->getQuery()->execute();
     }
 
@@ -54,7 +56,7 @@ class SessionRepository extends DocumentRepository
     {
         return $this->createQueryBuilder()->field('id')->equals($id)->field('statusId')->notIn(array(
             Session::STATUS_CANCELED,
-            Session::STATUS_CLOSED))->field('updatedAt')->gt(new MongoDate(time() - 300))->getQuery()->getSingleResult();
+            Session::STATUS_CLOSED))->getQuery()->getSingleResult();
     }
 
     public function getOpenInvites()
