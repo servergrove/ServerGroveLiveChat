b91466504d6b94c2bdd75e030d8b2d4e64716e44
iambrosi <ismaambrosi@gmail.com>
2011-03-02 21:05:33 -0200

    Root dir is set to sys_get_temp_dir()

--- src/ServerGrove/SGLiveChatBundle/Tests/TestKernel.php
+++ src/ServerGrove/SGLiveChatBundle/Tests/TestKernel.php
@@ -29,7 +29,7 @@ class TestKernel extends Kernel
 
     public function registerRootDir()
     {
-        return __DIR__;
+        return sys_get_temp_dir();
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
