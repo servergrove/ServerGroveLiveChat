4ab796834127eb0dd5baab81136cc0faebf9ae03
iambrosi <ismaambrosi@gmail.com>
2011-05-16 14:00:42 -0300

    init method added to AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,10 +1,26 @@
 <?php
 
+use Symfony\Component\ClassLoader\DebugUniversalClassLoader;
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
+    public function init()
+    {
+        if ($this->debug) {
+            ini_set('display_errors', 1);
+            error_reporting(-1);
+
+            DebugUniversalClassLoader::enable();
+            ErrorHandler::register();
+        } else {
+            ini_set('display_errors', 0);
+        }
+    }
+
     public function registerBundles()
     {
         $bundles = array(
@@ -17,10 +33,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             //new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new ServerGrove\SGLiveChatBundle\SGLiveChatBundle(),
-        );
+            new ServerGrove\SGLiveChatBundle\SGLiveChatBundle());
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array(
+            'dev',
+            'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Symfony\Bundle\WebConfiguratorBundle\SymfonyWebConfiguratorBundle();
         }
