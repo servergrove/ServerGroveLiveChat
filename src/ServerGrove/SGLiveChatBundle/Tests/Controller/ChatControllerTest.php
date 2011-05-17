611b88e3c2ac67ee83ee141bd4026a26feda94b2
iambrosi <ismaambrosi@gmail.com>
2011-05-17 09:45:46 -0300

    Update Tests

--- src/ServerGrove/SGLiveChatBundle/Tests/Controller/ChatControllerTest.php
+++ src/ServerGrove/SGLiveChatBundle/Tests/Controller/ChatControllerTest.php
@@ -3,7 +3,6 @@
 namespace ServerGrove\SGLiveChatBundle\Tests\Controller;
 
 use Symfony\Component\BrowserKit\Client;
-
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Cookie;
 
@@ -12,33 +11,6 @@ use Symfony\Component\HttpFoundation\Cookie;
  */
 class ChatControllerTest extends WebTestCase
 {
-
-    private function getDefaultLoginParams()
-    {
-        return array(
-            'chat' => array(
-                'name' => 'Ismael',
-                'email' => 'ismael@servergrove.com',
-                'question' => 'This is my comment'));
-    }
-
-    private function getDefaultLoginCompleteNameParams()
-    {
-        return array(
-            'chat[name]' => 'Ismael',
-            'chat[email]' => 'ismael@servergrove.com',
-            'chat[question]' => 'This is my comment');
-    }
-
-    protected function setUp()
-    {
-        parent::setUp();
-    }
-
-    protected function tearDown()
-    {
-        parent::tearDown();
-    }
 
     public function testIndexGet()
     {
@@ -52,8 +24,12 @@ class ChatControllerTest extends WebTestCase
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Question")')->count(), 'HTML not contains "Question"');
 
-        $client->submit($crawler->selectButton('Start Chat')->form(), $this->getDefaultLoginCompleteNameParams());
-        $this->assertTrue($client->getResponse()->isRedirect(), 'Is not redirecting');
+        $form = $crawler->selectButton('Start Chat')->form();
+        $this->fillLoginFormFields($form);
+
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirection(), 'Is not redirecting');
+        
         unset($client, $crawler);
     }
 
@@ -65,14 +41,15 @@ class ChatControllerTest extends WebTestCase
         /* @var $crawler Symfony\Component\DomCrawler\Crawler */
         $crawler = $client->request('GET', '/sglivechat');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'GET response not successful: ' . $client->getResponse()->getContent());
+        $form = $crawler->selectButton('Start Chat')->form();
+        $this->fillLoginFormFields($form);
 
-        $client->submit($crawler->selectButton('Start Chat')->form(), $this->getDefaultLoginCompleteNameParams());
+        $client->submit($form);
         $this->assertTrue($client->getResponse()->isRedirect(), 'Is not redirecting');
 
-        $crawler = $client->followRedirect();
-
-        $this->assertTrue($client->getResponse()->isRedirect(), 'Is not redirecting' . $client->getResponse()->getContent());
+        $crawler = $client->followRedirect();        
+        
+        $this->assertTrue($crawler->filter('a:contains("submit a support ticket")')->count() > 0);
         unset($client, $crawler);
     }
 
@@ -88,7 +65,7 @@ class ChatControllerTest extends WebTestCase
 
         /* @var $crawler Symfony\Component\DomCrawler\Crawler */
         $crawler = $client->request('GET', '/sglivechat/123whatever321/load');
-
+                
         $this->assertTrue($client->getResponse()->isRedirect(), 'Is not redirecting');
 
         $this->createSession($client);
@@ -96,14 +73,34 @@ class ChatControllerTest extends WebTestCase
         /* @var $crawler Symfony\Component\DomCrawler\Crawler */
         $crawler = $client->request('GET', '/sglivechat/' . $client->getRequest()->getSession()->get('chatsession') . '/load');
 
-        $this->assertTrue($client->getResponse()->isRedirect(), 'Is not redirecting' . $client->getResponse()->getContent());
+        $this->assertTrue($crawler->filter('a:contains("submit a support ticket")')->count() > 0);
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
     }
 
     private function createSession(Client $client)
     {
         /* @var $crawler Symfony\Component\DomCrawler\Crawler */
         $crawler = $client->request('GET', '/sglivechat');
-        $client->submit($crawler->selectButton('Start Chat')->form(), $this->getDefaultLoginCompleteNameParams());
+
+        $form = $crawler->selectButton('Start Chat')->form();
+        $this->fillLoginFormFields($form);
+
+        $client->submit($form);
     }
 
+    private function fillLoginFormFields($form)
+    {
+        $form['chatrequest[name]'] = 'Ismael Ambrosi';
+        $form['chatrequest[email]'] = 'ismael@servergrove.com';
+        $form['chatrequest[question]'] = 'This is my question';
+    }
 }
