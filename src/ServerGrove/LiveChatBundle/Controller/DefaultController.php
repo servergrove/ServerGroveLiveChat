dd744fbd29e45c9e5837b6b7b1b8878723161218
iambrosi <ismaambrosi@gmail.com>
2011-06-15 16:08:52 -0300

    Redirect default page to chat index

--- src/ServerGrove/LiveChatBundle/Controller/DefaultController.php
+++ src/ServerGrove/LiveChatBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ServerGroveLiveChatBundle:Default:index.html.twig');
+        return new \Symfony\Component\HttpFoundation\RedirectResponse($this->generateUrl('sglc_chat_homepage'));
     }
 }
