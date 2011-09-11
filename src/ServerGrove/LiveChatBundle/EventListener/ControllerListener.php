43183d89863f24e5109309cfea0ccfaaf6bb45b4
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-09-11 14:50:42 -0300

    Fixed cookie iteration

--- src/ServerGrove/LiveChatBundle/EventListener/ControllerListener.php
+++ src/ServerGrove/LiveChatBundle/EventListener/ControllerListener.php
@@ -12,18 +12,25 @@ class ControllerListener
 
     private $container;
 
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
+    /**
+     * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
+     * @return void
+     */
     public function onKernelResponse(FilterResponseEvent $event)
     {
         if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
             return;
         }
 
-        foreach ($this->container->get('livechat.cookies') as $cookie) {
+        foreach ($this->container->get('livechat.cookies')->all() as $cookie) {
             if ($cookie instanceof Cookie) {
                 $event->getResponse()->headers->setCookie($cookie);
             }
