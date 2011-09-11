a00b169769c251c0452175659ea84d39c84a9c94
Ismael Ambrosi <ismaambrosi@gmail.com>
2011-09-11 02:24:40 -0300

    Code cleaning

--- src/ServerGrove/LiveChatBundle/Document/SessionRepository.php
+++ src/ServerGrove/LiveChatBundle/Document/SessionRepository.php
@@ -20,53 +20,65 @@ class SessionRepository extends DocumentRepository
     public function getRequestedChats()
     {
         $qb = $this->createQueryBuilder();
-        $qb->addOr($qb->expr()->field('statusId')->equals(Session::STATUS_WAITING)->field('updatedAt')->range(new MongoDate(time() - (3600)), new MongoDate(time())))
-                ->addOr($qb->expr()->field('updatedAt')->range(new MongoDate(time() - 300), new MongoDate(time())));
+        $qb->addOr(
+            $qb->expr()
+                    ->field('statusId')->equals(Session::STATUS_WAITING)
+                    ->field('updatedAt')->range(new MongoDate(time() - (3600)), new MongoDate(time())
+            )
+        )->addOr(
+            $qb->expr()
+                    ->field('updatedAt')->range(new MongoDate(time() - 300), new MongoDate(time())
+            )
+        );
         $query = $qb->getQuery();
         return $query->execute();
     }
 
     public function getRequestedChatsArray()
     {
-        return array_map(
-                function (Session $chat) {
-                    $operator = array();
-                    if ($chat->getOperator()) {
-                        $operator['id'] = $chat->getOperator()->getId();
-                        $operator['name'] = $chat->getOperator()->getName();
-                    }
-                    return array(
-                        'id' => $chat->getId(),
-                        'visitor' => array(
-                            'id' => $chat->getVisitor()->getId(),
-                            'name' => $chat->getVisitor()->getName(),
-                            'email' => $chat->getVisitor()->getEmail()
-                        ),
-                        'question' => $chat->getQuestion(),
-                        'time' => $chat->getCreatedAt()->format('Y-m-d H:i:s'),
-                        'duration' => $chat->getUpdatedAt()->format('U') - $chat->getCreatedAt()->format('U'),
-                        'operator' => $operator,
-                        'status' => array(
-                            'id' => $chat->getStatusId(),
-                            'name' => $chat->getStatus()
-                        )
-                    );
-                }, $this->getRequestedChats()->toArray());
+        return array_map(function (Session $chat)
+            {
+                $operator = array();
+                if ($chat->getOperator()) {
+                    $operator['id'] = $chat->getOperator()->getId();
+                    $operator['name'] = $chat->getOperator()->getName();
+                }
+                return array(
+                    'id' => $chat->getId(),
+                    'visitor' => array(
+                        'id' => $chat->getVisitor()->getId(),
+                        'name' => $chat->getVisitor()->getName(),
+                        'email' => $chat->getVisitor()->getEmail()
+                    ),
+                    'question' => $chat->getQuestion(),
+                    'time' => $chat->getCreatedAt()->format('Y-m-d H:i:s'),
+                    'duration' => $chat->getUpdatedAt()->format('U') - $chat->getCreatedAt()->format('U'),
+                    'operator' => $operator,
+                    'status' => array(
+                        'id' => $chat->getStatusId(),
+                        'name' => $chat->getStatus()
+                    )
+                );
+            }, $this->getRequestedChats()->toArray());
     }
 
     public function closeSessions()
     {
-        $this->createQueryBuilder()->field('statusId')->set(Session::STATUS_CANCELED)->field('statusId')->notIn(array(
+        $status = array(
             Session::STATUS_CANCELED,
             Session::STATUS_WAITING,
-            Session::STATUS_CLOSED))->field('updatedAt')->lt(new MongoDate(time() - 300))->update()->getQuery()->execute();
+            Session::STATUS_CLOSED
+        );
+        $this->createQueryBuilder()->field('statusId')->set(Session::STATUS_CANCELED)->field('statusId')->notIn($status)->field('updatedAt')->lt(new MongoDate(time() - 300))->update()->getQuery()->execute();
     }
 
     public function getSessionIfNotFinished($id)
     {
-        return $this->createQueryBuilder()->field('id')->equals($id)->field('statusId')->notIn(array(
+        $status = array(
             Session::STATUS_CANCELED,
-            Session::STATUS_CLOSED))->getQuery()->getSingleResult();
+            Session::STATUS_CLOSED
+        );
+        return $this->createQueryBuilder()->field('id')->equals($id)->field('statusId')->notIn($status)->getQuery()->getSingleResult();
     }
 
     public function getOpenInvitesForVisitor(Visitor $visitor)
