64b414464d9e481b3640e0623bb64727fa22f7e2
Ismael Ambrosi <ismaambrosi@gmail.com>
2012-02-07 13:49:36 -0200

    removed vendor files

--- vendor/doctrine-common/tests/Doctrine/Tests/Common/Annotations/Fixtures/Annotation/Route.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Doctrine\Tests\Common\Annotations\Fixtures\Annotation;
-
-class Route extends \Doctrine\Common\Annotations\Annotation
-{
-    private $pattern;
-    private $name;
-}
--- vendor/doctrine-common/tests/Doctrine/Tests/Common/Annotations/TopLevelAnnotation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Doctrine\Common\Annotations\Annotation;
-
-class TopLevelAnnotation extends Annotation
-{
-}
