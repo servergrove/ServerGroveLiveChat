59dcb4da2045c7c31407676508c8d084abd79adc
Ismael Ambrosi <ismaambrosi@gmail.com>
2012-02-08 14:38:30 -0200

    JS Merge

---
+++ src/ServerGrove/LiveChatBundle/Resources/public/js/SGApi.js
@@ -0,0 +1,72 @@
+(function($) {
+    var sg = window.ServerGrove = {
+        Cookie: {
+            create: function(name, value, days) {
+                if (days) {
+                    var date = new Date();
+                    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                    var expires = "; expires=" + date.toGMTString();
+                } else {
+                    var expires = "";
+                }
+
+                document.cookie = name + "=" + value + expires + "; path=/";
+            },
+
+            read: function(name) {
+                var nameEQ = name + "=";
+                var ca = document.cookie.split(';');
+
+                for (var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+
+                    while (c.charAt(0) == ' ') {
+                        c = c.substring(1, c.length);
+                    }
+
+                    if (c.indexOf(nameEQ) == 0) {
+                        return c.substring(nameEQ.length, c.length);
+                    }
+                }
+
+                return null;
+            },
+
+            erase: function(name) {
+                sg.Cookie.create(name, "", -1);
+            }
+        },
+
+        Sound: {
+            /**
+             * Mute sound when = 1
+             */
+            _mute: -1,
+
+            /**
+             * Call flashsound swf to play desired sound
+             *
+             * @param el
+             * @param sound
+             */
+            play: function(el, sound) {
+                if (sg.Sound._mute == -1) {
+                    if (el) {
+                        if (typeof el.TGotoLabel != "undefined") {
+                            el.TGotoLabel("/", sound);
+                            el.TPlay("/");
+                        }
+                    }
+                }
+            },
+
+            /**
+             * Toggle sound mute state
+             */
+            mute: function() {
+                sg.Sound._mute *= -1;
+            }
+        }
+    };
+
+})(jQuery);
