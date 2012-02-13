212591dbb4178983b96bfa3b8e2579d3ba97eb46
Ismael Ambrosi <ismaambrosi@gmail.com>
2012-02-13 12:35:09 -0200

    Created Staff department on fixtures

--- src/ServerGrove/LiveChatBundle/DataFixtures/MongoDB/LoadUserData.php
+++ src/ServerGrove/LiveChatBundle/DataFixtures/MongoDB/LoadUserData.php
@@ -6,6 +6,7 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use ServerGrove\LiveChatBundle\Document\Administrator;
 use ServerGrove\LiveChatBundle\Document\Operator;
+use ServerGrove\LiveChatBundle\Document\OperatorDepartment;
 
 /**
  * Class LoadUserData
@@ -22,28 +23,35 @@ class LoadUserData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $this->createAdministrator($manager, 'Administrator', 'admin@example.com', 'testing');
-        $this->createOperator($manager, 'John Doe', 'john@example.com', 'testing');
-        $this->createOperator($manager, 'Jane Doe', 'jane@example.com', 'testing');
-        $this->createOperator($manager, 'Ismael Ambrosi', 'ismael@servergrove.com', 'testing');
+        $department = new OperatorDepartment();
+        $department->setIsActive(true);
+        $department->setName('Staff');
+        $manager->persist($department);
+        $manager->flush();
+
+        $this->createAdministrator($manager, 'Administrator', 'admin@example.com', 'testing', $department);
+        $this->createOperator($manager, 'John Doe', 'john@example.com', 'testing', $department);
+        $this->createOperator($manager, 'Jane Doe', 'jane@example.com', 'testing', $department);
+        $this->createOperator($manager, 'Ismael Ambrosi', 'ismael@servergrove.com', 'testing', $department);
 
         $manager->flush();
     }
 
-    private function createOperator(ObjectManager $manager, $name, $email, $passwd)
+    private function createOperator(ObjectManager $manager, $name, $email, $passwd, OperatorDepartment $department)
     {
-        return $this->saveUser($manager, new Operator(), $name, $email, $passwd);
+        return $this->saveUser($manager, new Operator(), $name, $email, $passwd, $department);
     }
 
-    private function createAdministrator(ObjectManager $manager, $name, $email, $passwd)
+    private function createAdministrator(ObjectManager $manager, $name, $email, $passwd, OperatorDepartment $department)
     {
-        return $this->saveUser($manager, new Administrator(), $name, $email, $passwd);
+        return $this->saveUser($manager, new Administrator(), $name, $email, $passwd, $department);
     }
 
-    private function saveUser(ObjectManager $manager, Operator $operator, $name, $email, $passwd)
+    private function saveUser(ObjectManager $manager, Operator $operator, $name, $email, $passwd, OperatorDepartment $department)
     {
         $operator->setName($name);
         $operator->setEmail($email);
+        $operator->addDepartment($department);
 
         $encoder = new \Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder();
         $operator->setPasswd($encoder->encodePassword($passwd, $operator->getSalt()));
