d9d714b8b4934a3b78709d2513670f7b6a49ea56
Mario Cuba <mario@mariocuba.net>
2013-05-06 18:37:33 -0430

    FormBuilder's now an interface.

--- src/ServerGrove/LiveChatBundle/Form/OperatorLoginType.php
+++ src/ServerGrove/LiveChatBundle/Form/OperatorLoginType.php
@@ -2,13 +2,13 @@
 
 namespace ServerGrove\LiveChatBundle\Form;
 
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
 
 class OperatorLoginType extends AbstractType
 {
 
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('email');
         $builder->add('passwd', 'password');
